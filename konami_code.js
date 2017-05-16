3b0e6df44b31b768f2ba501bde945b1ac5b1ce3c
kylewstewart <kyle.w.stewart@mac.com>
2017-05-16 16:05:59 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,32 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// const alphabet = ['a', 'b', 'c']
+
+
 
 function init() {
-  // your code here
+
+const doc = document.body.addEventListener('keydown', function(event){
+  onKeyDownHandler(event)
+})
+
+// keep track of index outside of the event handler
+var index = 0
+
+// unattached event handler
+function onKeyDownHandler(e) {
+  console.log(e);
+  const key = parseInt(e.detail || e.which);
+  console.log(key);
+
+  if (key === code[index]) {
+    index++
+
+    if (index === code.length - 1) {
+      alert("YOU DID IT!")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
+}
 }
